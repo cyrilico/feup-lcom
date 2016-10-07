ddf12e81b0c8d2c0c8f823c4beaacecdef98fbce
up201505836 <up201505836@a7b8acb8-61f2-49d0-8852-24440466c1fd>
2016-10-07 09:41:48 +0000

    Finish timer_get_conf (not tested)

--- lab2/timer.c
+++ lab2/timer.c
@@ -1,5 +1,6 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
+#include "i8254.h"
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
 
@@ -22,7 +23,29 @@ void timer_int_handler() {
 
 int timer_get_conf(unsigned long timer, unsigned char *st) {
 	
-	return 1;
+	unsigned long st_temp; /* because argument in sys_inb is of type unsigned long* and st is not of that type*/
+
+	switch(timer){
+		case 0:
+			sys_outb(TIMER_CTRL, 0xC2); /* Read-back, COUNT and STATUS active, Timer0 */
+			sys_inb(TIMER_0, &st_temp);
+			*st = static_cast<unsigned char> (st_temp);
+			return 0;
+		case 1:
+			sys_outb(TIMER_CTRL, 0xC4); /* Read-back, COUNT and STATUS active, Timer1 */
+			sys_inb(TIMER_1, &st_temp);
+			*st = static_cast<unsigned char> (st_temp);
+			return 0;
+		case 2:
+			sys_outb(TIMER_CTRL, 0xC8); /* Read-back, COUNT and STATUS active, Timer2 */
+			sys_inb(TIMER_2, &st_temp);
+			*st = static_cast<unsigned char> (st_temp);
+			return 0;
+		default:
+			return -1;
+
+	}
+
 }
 
 int timer_display_conf(unsigned char conf) {
