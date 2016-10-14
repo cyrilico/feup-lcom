21a18d604c9b59ec0e1da3d1a21fe20c9041bce2
up201505836 <up201505836@a7b8acb8-61f2-49d0-8852-24440466c1fd>
2016-10-14 09:00:33 +0000

    Fix timer_subscribe_int's typos and implement timer_unsubscribe_int (not tested)

--- lab2/timer.c
+++ lab2/timer.c
@@ -49,7 +49,8 @@ int timer_subscribe_int(void ) {
 	/*Bit specified in hookid to be returned by function if both calls are successful
 	 (variable needed because hookid will be modified in case of successful call)*/
 	int hookid_bit = BIT(hookid);
-	switch(sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hookid){
+
+	switch(sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hookid)) {
 	case 0: //Success in setpolicy call
 		if(!sys_irqenable(&hookid)) //Success in enable call
 			return hookid_bit;
@@ -61,7 +62,7 @@ int timer_subscribe_int(void ) {
 }
 
 int timer_unsubscribe_int() {
-	switch(sys_irqdisable( &hookid){
+	switch(sys_irqdisable( &hookid)) {
 	case 0: //Success in disable call
 		if(!sys_irqrmpolicy(&hookid)) //Success in unsubscribe call
 			return 0;
@@ -135,7 +136,40 @@ int timer_test_square(unsigned long freq) {
 
 int timer_test_int(unsigned long time) {
 	
-	return 1;
+	int r;
+	int irq_set = timer_subscribe_int();
+
+	if(irq_set == -1) //Failed subscription
+		return -1;
+
+	int ipc_status;
+	message msg;
+
+	while(interrupt_counter/60 < time) { /* You may want to use a different condition */
+		/* Get a request message. */
+		if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+			printf("driver_receive failed with: %d", r);
+			continue;
+		}
+		if (is_ipc_notify(ipc_status)) { /* received notification */
+			switch (_ENDPOINT_P(msg.m_source)) {
+			case HARDWARE: /* hardware interrupt notification */
+				if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+					timer_int_handler();
+					if(interrupt_counter % 60 == 0)
+						printf("Hey! Another second has passed. %d seconds gone so far\n", interrupt_counter/60);
+
+				}
+				break;
+			default:
+				break; /* no other notifications expected: do nothing */
+			}
+		} else { /* received a standard message, not a notification */
+			/* no standard messages expected: do nothing */
+		}
+	}
+
+	return timer_unsubscribe_int();
 }
 
 int timer_test_config(unsigned long timer) {
