79a77dd13830e96966704f316a93165c5c34c641
António Almeida <up201505836@fe.up.pt>
2016-10-14 08:43:03 +0000

    Fix timer_subscribe_int's typos and implement timer_unsubscribe_int (not tested)

--- lab2/timer.c
+++ lab2/timer.c
@@ -47,11 +47,11 @@ int timer_set_square(unsigned long timer, unsigned long freq) {
 
 int timer_subscribe_int(void ) {
 	/*Bit specified in hookid to be returned by function if both calls are successful
-	 (variable needed because hookid will me modified in case of successful call)*/
+	 (variable needed because hookid will be modified in case of successful call)*/
 	int hookid_bit = BIT(hookid);
-	switch(sys_setpolicy(TIMER0_IRQ, IRQ_REENABLE, &hookid){
-	case 0: //Sucess in setpolicy call
-		if(!sys_irqenable(&hookid)) //Sucess in enable call
+	switch(sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hookid){
+	case 0: //Success in setpolicy call
+		if(!sys_irqenable(&hookid)) //Success in enable call
 			return hookid_bit;
 		else
 			return -1;
@@ -61,7 +61,15 @@ int timer_subscribe_int(void ) {
 }
 
 int timer_unsubscribe_int() {
-	return 1;
+	switch(sys_irqdisable( &hookid){
+	case 0: //Success in disable call
+		if(!sys_irqrmpolicy(&hookid)) //Success in unsubscribe call
+			return 0;
+		else
+			return -1;
+	default: //Unsuccessful call, no need to do unsubscribe call
+		return -1;
+	}
 }
 
 void timer_int_handler() {
